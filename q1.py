e2a2c80a3cb4b06d571aa4bb67aac2f353855a42
Conrad MAC <conradyoung1021@gmail.com>
2022-10-14 16:15:16 +0800

    final

--- q1.py
+++ q1.py
@@ -1,31 +1,26 @@
-num = int(input())
+n = int(input())
 dig = int(input())
 count = 0
 
-if num < 1000 and num > 0:
+if n < 1000 and n > 0:
     if dig <= 10 and dig >= 1:
-        for i in range(1, num+1):
-#check num between 1 to 99
-            if i <= 99:
-                check_tens = i//10
-                check_ones = i % 10
-                if check_tens == dig:
+        for i in range(1, n + 1):
+            num = list(str(i))
+            if  i < 10:
+                if num[0] == str(dig):
                     count += 1
-                if check_ones == dig:
+            elif i < 100:
+                if num[0] == str(dig):
                     count += 1
-
-#check num bigger or eqaul to 100 but less than 1000
-            elif i < 1000:
-                check_huns = i//100
-                check_tens = check_huns % 10
-                check_ones = i % 100
-
-                if check_huns == dig:
+                if num[1] == str(dig):
                     count += 1
-                if check_tens == dig:
+            elif i <= 1000:
+                if num[0] == str(dig):
                     count += 1
-                if check_ones == dig:
+                if num[1] == str(dig):
                     count += 1
-    print(count)
+                if num[2] == str(dig):
+                    count += 1
 else:
     quit()
+print(count)
