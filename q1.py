a59e28b55bcbb891a2381ad2200a7121d7c48805
Conrad MAC <conradyoung1021@gmail.com>
2022-10-14 16:18:27 +0800

    final

--- q1.py
+++ q1.py
@@ -3,7 +3,7 @@ dig = int(input())
 count = 0
 
 if n < 1000 and n > 0:
-    if dig <= 10 and dig >= 1:
+    if dig <= 9 and dig >= 1:
         for i in range(1, n + 1):
             num = list(str(i))
             if  i < 10:
