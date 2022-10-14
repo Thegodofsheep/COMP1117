af7621aecc5d7654d848e50041a44d70a0d730e7
Conrad MAC <conradyoung1021@gmail.com>
2022-10-14 18:56:31 +0800

    q3p1

---
+++ q3_p1.py
@@ -0,0 +1,24 @@
+#PART 0
+mode = int(input())
+if mode != 1 and mode != 2 and mode != 3:
+    print("Invalid mode")
+    quit()
+#################################################################
+#PART 1
+height = int(input())
+width = int(input())
+
+if height <= 0:
+    print("Invalid height of candles")
+    quit()
+if width <= 0:
+    print("Invalid number of candles")
+    quit()
+
+for i in range(0,width-1):
+    print("[", end='')
+print("[")
+for i in range(0, height):
+    for i in range(0,width):
+        print("[]", end="")
+        break
