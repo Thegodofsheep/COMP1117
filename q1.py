46137fe0b59d7f41c145646fb0380b9e6f484b68
Conrad MAC <conradyoung1021@gmail.com>
2022-10-14 15:51:31 +0800

    tets

--- q1.py
+++ q1.py
@@ -4,15 +4,28 @@ count = 0
 
 if num < 1000 and num > 0:
     if dig <= 10 and dig >= 1:
-        for i in range(1,num+1):
-            check_1 = i//10
-            check_2 = i%10
-            if check_1 == dig:
-                count += 1
-            if check_2 == dig:
-                count += 1
+        for i in range(1, num+1):
+#check num between 1 to 99
+            if i <= 99:
+                check_tens = i//10
+                check_ones = i % 10
+                if check_tens == dig:
+                    count += 1
+                if check_ones == dig:
+                    count += 1
+
+#check num bigger or eqaul to 100 but less than 1000
+            elif i >= 100:
+                check_huns = i//100
+                check_tens = check_huns % 10
+                check_ones = i % 100
+
+                if check_huns == dig:
+                    count += 1
+                if check_tens == dig:
+                    count += 1
+                if check_ones == dig:
+                    count += 1
+    print(count)
 else:
     quit()
-print(count)
-
-
